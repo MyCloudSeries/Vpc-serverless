e4989033a656bd35b6e05705cfba3998bf36bdbf
Ayinde Jamiu <ayindejamiu90@gmail.com>
2022-03-29 16:38:32 +0100

    this added s3

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "project_public_subnet" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.123.1.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-west-1a"
   tags = {
     Name = "main"
   }
