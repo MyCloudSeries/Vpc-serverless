29c70e7492e27c5d795d0e42047d2370e80b4ebd
ayinde.jamiu <ayindejamiu90@gmal.com>
2022-04-15 08:21:50 +0100

    changes

--- main.tf
+++ main.tf
@@ -12,6 +12,15 @@ resource "aws_subnet" "project_public_subnet" {
     Name = "main"
   }
 }
+resource "aws_subnet" "project_public_subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-1a"
+  tags = {
+    Name = "main"
+  }
+}
 
 resource "aws_internet_gateway" "project_gw" {
   vpc_id = aws_vpc.main.id
