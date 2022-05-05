656c922c404b7b8dba50fb322b3527ba21a596c7
ayinde.jamiu <ayindejamiu90@gmal.com>
2022-05-05 09:26:10 +0100

    adjustment of s3 bucket and others

--- var.tf
+++ var.tf
@@ -1,15 +1,3 @@
-variable "coldsis_website" {
-  type        = string
-  default     = "www.coldsis.com"
-  description = "this is the frontend URL"
-}
-
-variable "bucket_url" {
-  type        = string
-  default     = ""
-  description = "this is the bucket url"
-}
-
 variable "bucket_name" {
   type        = string
   default     = "coldsis"
